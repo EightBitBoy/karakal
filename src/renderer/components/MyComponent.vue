b9944d311e5fd487e33e2e9ab196903f08b910d0
Stefan Härtel <haertel.dev@gmail.com>
2018-05-25 00:27:11 +0200

    Add components

---
+++ src/renderer/components/MyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Hello world!</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: 'This is a message!'
+    }
+  }
+}
+</script>
+
+<style>
+h2 {
+    color: blue;
+}
+</style>
