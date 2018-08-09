7f8f63f877cefa683efc2b6210fe2820fdb09bfb
Stefan Härtel <haertel.dev@gmail.com>
2018-08-09 23:41:40 +0200

    Prepare new component

--- src/renderer/components/MainComponent.vue
+++ src/renderer/components/MainComponent.vue
@@ -3,15 +3,19 @@
     <div>
       <hello-world/>
     </div>
+    <div>
+      <transaction-list/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './HelloWorld'
+import TransactionList from './TransactionList'
 
 export default {
   name: 'MainComponent',
-  components: {HelloWorld}
+  components: {HelloWorld, TransactionList}
 }
 </script>
 
