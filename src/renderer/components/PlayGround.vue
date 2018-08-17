8551ceb29b0ee453c8f9458cd9bf0f39bbc8e18e
Stefan Härtel <haertel.dev@gmail.com>
2018-08-18 00:11:27 +0200

    Add list component

--- src/renderer/components/PlayGround.vue
+++ src/renderer/components/PlayGround.vue
@@ -3,6 +3,7 @@
     <div>
       <hello-world/>
       <value-counter/>
+      <item-list/>
     </div>
   </div>
 </template>
@@ -10,10 +11,11 @@
 <script>
 import HelloWorld from './PlayGround/HelloWorld'
 import ValueCounter from './PlayGround/ValueCounter'
+import ItemList from './PlayGround/ItemList'
 
 export default {
   name: 'PlayGround',
-  components: {HelloWorld, ValueCounter}
+  components: {HelloWorld, ValueCounter, ItemList}
 }
 </script>
 
