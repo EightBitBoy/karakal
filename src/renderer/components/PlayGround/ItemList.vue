bfbd5b5a5ff08d75c3e907b9404ff13a1e4262dc
Stefan Härtel <haertel.dev@gmail.com>
2018-08-18 00:11:44 +0200

    Add list component

---
+++ src/renderer/components/PlayGround/ItemList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>ItemList</h1>
+    <ul>
+      <li v-for="item in items" v-bind:key="item">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemList',
+  data: function(){
+    return {
+      items: ['Apple', 'Banana', 'Cucumber', 'Dragonfruit']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
